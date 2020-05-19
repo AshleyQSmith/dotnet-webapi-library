d8347abdb7cd3e90746790c00a83b41cb75b3fcc
AshleyQSmith <smittyq@gmail.com>
2020-05-19 08:01:46 -0600

    Dapper & mySqlConnector added

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -1,10 +1,15 @@
 using dotnet_webapi_library.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace dotnet_webapi_library.Controllers
 {
   [ApiController]
-  [Route("api/[controller]")
+  [Route("api/[controller]")]
   public class LibraryController : ControllerBase
   {
     private readonly LibraryService _ls;
